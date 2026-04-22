1b3e4ab4f014192f2d1b377270184f66ebef7b90
shencan03 <shencan03@gmail.com>
2026-04-22 17:09:50 +0300

    feat: complete js element creation for each book in the books array, no css

--- index.js
+++ index.js
@@ -16,6 +16,40 @@ const appendBook = (title, author, pages, read) => {
   books.push(newBook);
 };
 
+// const iterateBooksArray = () => {
+//   for(let i = 0; i < books.length; i++){
+
+//   }
+// }
+
 appendBook("harry potter", "J.K Rowling", 100, false);
+
+const booksContainer = document.querySelector(".books-container");
+
+for (let i = 0; i < books.length; i++) {
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+  bookCard.id = books[i].id;
+
+  const bookTitle = document.createElement("h3");
+  bookTitle.classList.add("book-title");
+  bookTitle.textContent = books[i].title;
+  bookCard.appendChild(bookTitle);
+
+  const line = document.createElement("hr");
+  bookCard.appendChild(line);
+
+  const bookAuthor = document.createElement("p");
+  bookAuthor.classList.add("book-author");
+  bookAuthor.textContent = `by ${books[i].author}`;
+  bookCard.appendChild(bookAuthor);
+
+  const bookPages = document.createElement("span");
+  bookPages.classList.add("book-pages");
+  bookPages.textContent = books[i].pages;
+  bookCard.appendChild(bookPages);
+
+  booksContainer.appendChild(bookCard);
+}
 
 console.log(books[0]);
