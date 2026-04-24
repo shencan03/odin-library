af6c936e75c79ecd05d911337367586b45892ce4
shencan03 <shencan03@gmail.com>
2026-04-24 05:09:34 +0300

    feat: add new book complete without input error/logic checking

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function Book(title, author, pages, read) {
 const appendBook = (title, author, pages, read) => {
   const newBook = new Book(title, author, pages, read);
   books[newBook.id] = newBook;
+
+  return newBook.id;
 };
 
 appendBook("Harry Potter", "J.K Rowling", 100, false);
@@ -73,14 +75,13 @@ const fillBtnCard = () => {
   bookTitle.classList.add("book-title");
   bookTitle.classList.add("placeholder");
   bookTitle.textContent = "Title..";
-  bookTitle.addEventListener(
-    "keydown",
-    (e) => {
-      e.currentTarget.textContent = "";
-      e.currentTarget.classList.remove("placeholder");
-    },
-    { once: true },
-  );
+
+  const removePlaceHolder = (e) => {
+    e.currentTarget.textContent = "";
+    e.currentTarget.classList.remove("placeholder");
+  };
+
+  bookTitle.addEventListener("keydown", removePlaceHolder, { once: true });
   bookBtnCard.appendChild(bookTitle);
   bookTitle.focus();
 
@@ -92,14 +93,7 @@ const fillBtnCard = () => {
   bookAuthor.classList.add("book-author");
   bookAuthor.classList.add("placeholder");
   bookAuthor.textContent = "Author..";
-  bookAuthor.addEventListener(
-    "keydown",
-    (e) => {
-      e.currentTarget.textContent = "";
-      e.currentTarget.classList.remove("placeholder");
-    },
-    { once: true },
-  );
+  bookAuthor.addEventListener("keydown", removePlaceHolder, { once: true });
   bookBtnCard.appendChild(bookAuthor);
 
   const bookPages = document.createElement("span");
@@ -107,14 +101,7 @@ const fillBtnCard = () => {
   bookPages.classList.add("book-pages");
   bookPages.classList.add("placeholder");
   bookPages.textContent = "123..";
-  bookPages.addEventListener(
-    "keydown",
-    (e) => {
-      e.currentTarget.textContent = "";
-      e.currentTarget.classList.remove("placeholder");
-    },
-    { once: true },
-  );
+  bookPages.addEventListener("keydown", removePlaceHolder, { once: true });
   bookBtnCard.appendChild(bookPages);
 
   const svgNS = "http://www.w3.org/2000/svg";
@@ -132,6 +119,17 @@ const fillBtnCard = () => {
   );
   confirmPath.setAttribute("fill", "currentColor");
   bookConfirmBtn.appendChild(confirmPath);
+  bookConfirmBtn.addEventListener("click", () => {
+    const bookId = appendBook(
+      bookTitle.textContent,
+      bookAuthor.textContent,
+      bookPages.textContent,
+      false,
+    );
+    document.getElementById("button-card").remove();
+    appendBookCard(books[bookId]);
+    appendBtnCard();
+  });
   bookEdit.appendChild(bookConfirmBtn);
 
   const bookCancelBtn = document.createElementNS(svgNS, "svg");
@@ -143,6 +141,10 @@ const fillBtnCard = () => {
     "M12 2C17.5 2 22 6.5 22 12S17.5 22 12 22 2 17.5 2 12 6.5 2 12 2M12 4C10.1 4 8.4 4.6 7.1 5.7L18.3 16.9C19.3 15.5 20 13.8 20 12C20 7.6 16.4 4 12 4M16.9 18.3L5.7 7.1C4.6 8.4 4 10.1 4 12C4 16.4 7.6 20 12 20C13.9 20 15.6 19.4 16.9 18.3Z",
   );
   cancelPath.setAttribute("fill", "currentColor");
+  bookCancelBtn.addEventListener("click", () => {
+    document.getElementById("button-card").remove();
+    appendBtnCard();
+  });
   bookCancelBtn.appendChild(cancelPath);
   bookEdit.appendChild(bookCancelBtn);
 
